cfc4fbbc1007144ee667a00b2f6e6126c9f286dd
clovisjcrosa <clovisjcrosa@gmail.com>
2025-07-02 19:24:38 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Ativa CORS para qualquer origem (pode restringir depois se quiser)
+app.use(cors({
+  origin: 'https://www.agenciaupcom.com.br'
+}));
+
+// Rota de teste
 app.get('/', (req, res) => {
-  res.send('API do Encarte App funcionando!');
+  res.send('API do Encarte App funcionando com CORS!');
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor backend iniciado na porta ${PORT}`);
 });
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://agenciaupcom.com.br'
-}));
